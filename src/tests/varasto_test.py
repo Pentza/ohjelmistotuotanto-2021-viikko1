a8aa26d73b995b5188ebff4e0880a264c65eb70f
Pentti Lähdeaho <pentti.lahdeaho@gmail.com>
2021-11-04 13:23:00 +0200

    varasto poetry files

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,38 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_lisaa_liikaa_varasto_menee_tayteen(self):
+        self.varasto.lisaa_varastoon(self.varasto.paljonko_mahtuu() + 1)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_otetaan_liikaa_tilavuus_nolla(self):
+        self.varasto.ota_varastosta(self.varasto.saldo + 1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_varasto_print_toimii(self):
+        self.assertAlmostEqual(str(self.varasto), f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}")
+
+    def test_ota_negatiivinen_ei_vahenna(self):
+        self.varasto.ota_varastosta(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisaa_negatiivinen_ei_lisaa(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_alustus_negatiivisella_on_nolla(self):
+        uusi = Varasto(-1)
+
+        self.assertAlmostEqual(uusi.tilavuus, 0)
+
+    def test_negatiivinen_alku_saldo(self):
+        uusi = Varasto(10, -1)
+
+        self.assertAlmostEqual(uusi.paljonko_mahtuu(), 10)
+
+    
